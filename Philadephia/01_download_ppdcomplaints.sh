f60eacfaeb899264978041345b4dae164c323591
Xin Yi Li <lixiny0818@gmail.com>
2021-06-22 11:35:09 -0400

    Added PHI and download script

--- Philadephia/01_download_ppdcomplaints.sh
+++ Philadephia/01_download_ppdcomplaints.sh
@@ -1,7 +1,7 @@
 # 1. Download the Philadelphia data file
-curl https://raw.githubusercontent.com/sdl60660/philly_police_complaints/master/raw_data/ppd_complaint_complainants_7-18_corrected.csv
-curl https://raw.githubusercontent.com/sdl60660/philly_police_complaints/master/raw_data/ppd_complaint_disciplines_7-18.csv
-curl https://github.com/sdl60660/philly_police_complaints/blob/master/raw_data/ppd_complaints_7-18.csv?raw=true
+wget https://raw.githubusercontent.com/sdl60660/philly_police_complaints/master/raw_data/ppd_complaint_complainants_7-18_corrected.csv
+wget https://raw.githubusercontent.com/sdl60660/philly_police_complaints/master/raw_data/ppd_complaint_disciplines_7-18.csv
+wget https://raw.githubusercontent.com/sdl60660/philly_police_complaints/master/raw_data/ppd_complaints_7-18.csv
 
 # 2. Keep each one
 touch ppd_complaint_complainants_7-18_corrected.csv
