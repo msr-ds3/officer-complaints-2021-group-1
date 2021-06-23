eef435c708a8aedb4b0d04ec102b78df91c7151a
printmatt <mattvang@vang.localdomain>
2021-06-23 15:41:04 -0400

    analyze differences in race

--- newyork/03_plot_newyork_complaints.R
+++ newyork/03_plot_newyork_complaints.R
@@ -23,10 +23,27 @@ distinct_complaints <- officers_complaints %>% group_by(OfficerID) %>%
   summarise(num_complaints = n()) %>% ungroup() %>%
   mutate(decile = ntile(num_complaints,10))
 
-
+### plotting
 distinct_complaints %>%
   group_by(decile) %>%
   summarise(total_complaints = sum(num_complaints)) %>%
   mutate(percent_of_total = total_complaints/sum(total_complaints)) %>%
   ggplot(aes(x=decile,y=percent_of_total)) + geom_histogram(stat = 'identity') +
   ylab("Proportion of complaints") + xlab("Decile")
+
+#############################################################
+
+### compare ratio of same race complaints vs different race
+racial_differences <- ny_complaints %>% filter(ImpactedRace != "", OfficerRace != "") %>% count(ImpactedRace,OfficerRace)
+
+racial_differences %>%
+  mutate(same_race = if_else(ImpactedRace == OfficerRace,'Same Race','Different Race')) %>%
+  ggplot(aes(x=reorder(ImpactedRace,n), y = n)) +
+  geom_bar(aes(fill=OfficerRace),stat='identity') +
+  facet_wrap(~ same_race) +
+  xlab('Complainant Race') +
+  ylab('Number of complaints')
+
+
+
+
